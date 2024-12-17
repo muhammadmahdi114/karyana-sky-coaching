fc1db7163c2f1f5eb16990522b8d57df015dbade
Muhammad Mahdi <mahdimuhammad116@gmail.com>
2024-12-17 13:57:27 +0500

    Header Fix

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -12,7 +12,6 @@ export default function Header() {
         const date = new Date();
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
         setCurrentDate(date.toLocaleDateString(undefined, options));
-        console.log("USER: ", user);
     }, [user]);
 
     if (!user) {
@@ -24,26 +23,9 @@ export default function Header() {
 
             <div className="flex justify-between items-center">
                 <div>
-                    <h2 className="font-bold text-2xl">Welcome, {user.username}!</h2>
+                    <h2 className="font-bold text-2xl">Welcome, Admin!</h2>
                     <p className="text-gray-500">{currentDate}</p>
                 </div>
-                <div className="flex gap-x-2 items-center justify-center mr-5">
-                    <img src="/notification.png" alt="Notification" className="h-10" />
-                    <div className="flex ">
-                        <div>
-                            <img
-                                src={user.userImage}
-                                alt={user.username}
-                                className="w-12 h-12 mr-4"
-                            />
-                        </div>
-                        <div>
-                            <h2 className="font-bold">{user.username}</h2>
-                            <p className="text-gray-500 text-sm">Admin</p>
-                        </div>
-                    </div>
-                </div>
-
             </div>
         </div>
     )
